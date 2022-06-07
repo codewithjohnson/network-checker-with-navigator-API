35e3c4422d8e438fb10399c878e09a412718ac14
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-06-07 13:54:56 +0100

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,6 @@
 const body = document.querySelector('body');
 const STATUS = document.querySelector('.status');
 
-if (navigator.onLine){
-    online();
-}else{
-    offline();
-}
-
 function online() {
     body.style.backgroundColor = `deepskyblue`;
     STATUS.innerHTML = `<span> online</span>`;
@@ -16,4 +10,11 @@ function online() {
 function offline() {
     body.style.backgroundColor = `lightgreen`;
     STATUS.innerHTML = `<span> offline</span>`;
+}
+
+
+if (navigator.onLine){
+    online();
+}else{
+    offline();
 }
